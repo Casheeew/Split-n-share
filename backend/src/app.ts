66c00ff7069abb18a58313c72626265ebe4449ca
zoey <134000549+zoeyintheclouds@users.noreply.github.com>
2024-11-21 08:27:01 +0900

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -60,7 +60,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/search', searchRoutes);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join('../frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'));
   });
 
 // handle undefined Routes
