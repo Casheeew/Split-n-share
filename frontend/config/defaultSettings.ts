45115c8fb2713871b36d8de18e6a18f31b7cf7ea
Mai Nguyen <mainguyenngoc513@gmail.com>
2024-11-21 09:33:22 +0900

    fix url

--- frontend/config/defaultSettings.ts
+++ frontend/config/defaultSettings.ts
@@ -17,7 +17,7 @@ const Settings: ProLayoutProps & {
   colorWeak: false,
   title: 'Split-n-Share',
   pwa: true,
-  logo: 'frontend/public/split-n-share-logo.jpg',
+  logo: 'https://i.postimg.cc/qBYCht0x/split-n-share-icon.jpg',
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
