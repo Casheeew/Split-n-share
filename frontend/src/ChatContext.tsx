293964a7f261f69cd8f18cd062a469618a251ceb
Cashew <tungbinem@gmail.com>
2024-12-12 09:20:37 +0900

    commit

--- frontend/src/ChatContext.tsx
+++ frontend/src/ChatContext.tsx
@@ -1,12 +1,13 @@
 import { createContext, useState } from "react";
 
-export const ChatContext = createContext({ chatOpen: false, setChatOpen: (value: any) => { } });
+export const ChatContext = createContext({ chatOpen: false, setChatOpen: (value: any) => { }, selectedChatId: '', setSelectedChatId: (value: any) => { } });
 
 export const ChatContextProvider: React.FC<{ children: any }> = ({ children }) => {
   const [chatOpen, setChatOpen] = useState(false);
+  const [selectedChatId, setSelectedChatId] = useState('');
 
   return (
-    <ChatContext.Provider value={{ chatOpen, setChatOpen }}>
+    <ChatContext.Provider value={{ chatOpen, setChatOpen, selectedChatId, setSelectedChatId }}>
       {children}
     </ChatContext.Provider>
   );
